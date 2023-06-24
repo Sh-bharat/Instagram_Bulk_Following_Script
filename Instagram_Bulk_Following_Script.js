b827281c995196f1a5f33e37d043f9f12f8766b7
Bharat <133742551+Sh-bharat@users.noreply.github.com>
2023-06-24 12:55:36 +0530

    Add files via upload

---
+++ Instagram_Bulk_Following_Script.js
@@ -0,0 +1,17 @@
+const sleep = async (milliseconds) => {
+    await new Promise(resolve => {
+        return setTimeout(resolve, milliseconds)
+    });
+};
+list1=document.getElementsByClassName("_acan _acap _acas _aj1-")
+const gainFollowing = async () => {
+    for (let i = 0; i < list1.length; i++) {
+        await sleep(4000);
+        list1[1].click()
+    }
+
+    console.log("The loop is finished :)");
+}
+
+
+// gainFollowing();
